3544c1c452518187a9ddf5e79ae115a1c25fc927
Mari Helgesen <mahelges@stud.ntnu.no>
2024-01-09 11:04:12 +0100

    Light intensity and comments

--- environment/environment.ino
+++ environment/environment.ino
@@ -1,18 +1,41 @@
+// Remember to check the ports
+
 #include <dht11.h>
+
+// Define the correct ports
 #define DHT11PIN 2
 #define LED_humid 5
 #define LED_temp 10
+#define LED_light 3
+#define PHOTO_RESISTOR A0
 
 dht11 DHT11;
 
+// Set pinmodes for the LEDs and start the serial monitor.
 void  setup()
 {
   Serial.begin(115200);
   pinMode(LED_humid, OUTPUT);
   pinMode(LED_temp, OUTPUT);
- 
+  pinMode(LED_light, OUTPUT);
 }
 
+// Checks the light intensity with a photo resistor and raises alarm if value is too high.
+void checkLightIntensity() {
+  int value = analogRead(PHOTO_RESISTOR);
+
+  Serial.println("Analog  Value: ");
+  Serial.println(value);
+    
+  if (value > 900) {
+    digitalWrite(LED_light, HIGH);
+  } else {
+    digitalWrite(LED_light,  LOW);
+  }
+}
+
+// Uses the humidity sensor to check both temperature and humidity. 
+// Raises alarm if either is outside acceptable interval.
 void checkTempAndHumidity() {
   Serial.println();
   int chk = DHT11.read(DHT11PIN);
@@ -40,7 +63,9 @@ void checkTempAndHumidity() {
 
 void loop()
 {
+  checkLightIntensity();
   checkTempAndHumidity();
-  delay(2000);
-
+  
+  // Check the values every second.
+  delay(1000);
 }
