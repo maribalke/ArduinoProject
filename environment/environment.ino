6fe9066f8f3a9d3fc3c224c3f7d170a62454a21d
mahelges <122898552+mahelges@users.noreply.github.com>
2024-01-10 14:39:47 +0100

    Update environment.ino

--- environment/environment.ino
+++ environment/environment.ino
@@ -1,25 +1,14 @@
 // Remember to check the ports
+// Make sure the 3V3 and GND wires are connected
 
 #include <dht11.h>
 
 // Define the correct ports
-#define DHT11PIN D2
-#define LED_humid 5
-#define LED_temp 10
-#define LED_light 3
+#define DHT11PIN D7
+#define LED_HUMID D1
+#define LED_TEMP D2
+#define LED_LIGHT D4
 #define PHOTO_RESISTOR A0
-
-const int VIN = 5.0;
-const int R_light = 10000.0;
-
-int sensorRawToPhys(int aValue){
-  // Code from https://www.aranacorp.com/en/luminosity-measurement-with-a-photoresistor/
-  // Conversion rule
-  float Vout = float(aValue) * (VIN / float(1023));// Conversion analog to voltage
-  float RLDR = (R_light * (VIN - Vout))/Vout; // Conversion voltage to resistance
-  int phys=500/(RLDR/1000); // Conversion resitance to lumen
-  return phys;
-}
 
 dht11 DHT11;
 
@@ -27,9 +16,9 @@ dht11 DHT11;
 void  setup()
 {
   Serial.begin(115200);
-  pinMode(LED_humid, OUTPUT);
-  pinMode(LED_temp, OUTPUT);
-  pinMode(LED_light, OUTPUT);
+  pinMode(LED_HUMID, OUTPUT);
+  pinMode(LED_TEMP, OUTPUT);
+  pinMode(LED_LIGHT, OUTPUT);
 }
 
 // Checks the light intensity with a photo resistor and raises alarm if value is too high.
@@ -38,15 +27,11 @@ void checkLightIntensity() {
 
   Serial.println("Analog  Value: ");
   Serial.println(value);
-
-  float lux = sensorRawToPhys(value);     
-  Serial.println("Intensity in Lumen: ");
-  Serial.println(lux);
     
   if (value > 900) {
-    digitalWrite(LED_light, HIGH);
+    digitalWrite(LED_LIGHT, HIGH);
   } else {
-    digitalWrite(LED_light,  LOW);
+    digitalWrite(LED_LIGHT,  LOW);
   }
 }
 
@@ -59,15 +44,15 @@ void checkTempAndHumidity() {
   float temp = (float)DHT11.temperature;
 
   if (humidity > 65 || humidity < 40){
-    digitalWrite(LED_humid, HIGH);
+    digitalWrite(LED_HUMID, HIGH);
   } else {
-    digitalWrite(LED_humid, LOW);
+    digitalWrite(LED_HUMID, LOW);
   }
 
-  if (temp > 25 || temp < 20){
-    digitalWrite(LED_temp, HIGH);
+  if (temp > 27 || temp < 15){
+    digitalWrite(LED_TEMP, HIGH);
   } else {
-    digitalWrite(LED_temp, LOW);
+    digitalWrite(LED_TEMP, LOW);
   }
 
   Serial.print("Humidity (%): ");
