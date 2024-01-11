6fe53144e42abbdb8ae2e3d3b13e03aa5a5e4e99
mahelges <122898552+mahelges@users.noreply.github.com>
2024-01-11 10:14:15 +0100

    Added thingspeak to environment

--- environment/environment.ino
+++ environment/environment.ino
@@ -2,6 +2,15 @@
 // Make sure the 3V3 and GND wires are connected
 
 #include <dht11.h>
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+
+const char* ssid = "Iphonemari";
+const char* pass = "mari1234";
+WiFiClient client;
+unsigned long channelID = 2399509;  //your TS channal
+const char* APIKey = "735H239HRB9DNBFA"; // Replace with your Write API Keyconst char* APIKeyRead = "CYB6IXO89M7E5RFD";   //your reading API
+const char* server = "api.thingspeak.com";
 
 // Define the correct ports
 #define DHT11PIN D7
@@ -19,21 +28,46 @@ void  setup()
   pinMode(LED_HUMID, OUTPUT);
   pinMode(LED_TEMP, OUTPUT);
   pinMode(LED_LIGHT, OUTPUT);
+<<<<<<< HEAD
+  WiFi.begin(ssid, pass);
+}
+
+const int VIN = 5.0;
+const int R_light = 10000.0;
+
+int sensorRawToPhys(int aValue){
+  // Code from https://www.aranacorp.com/en/luminosity-measurement-with-a-photoresistor/
+  // Conversion rule
+  float Vout = float(aValue) * (VIN / float(1023));// Conversion analog to voltage
+  float RLDR = (R_light * (VIN - Vout))/Vout; // Conversion voltage to resistance
+  int phys=500/(RLDR/1000); // Conversion resitance to lumen
+  return phys;
+=======
+>>>>>>> 135e296618deba5328bfe0e2a93004308c3bc022
 }
 
 // Checks the light intensity with a photo resistor and raises alarm if value is too high.
 void checkLightIntensity() {
   int value = analogRead(PHOTO_RESISTOR);
 
-  Serial.println("Analog  Value: ");
-  Serial.println(value);
+  float lux = sensorRawToPhys(value);     
+  Serial.println("Light intensity in Lux: ");
+  Serial.println(lux);
+
+  ThingSpeak.setField(6, lux);
     
+<<<<<<< HEAD
+  if (lux > 400 || lux < 0) {
+=======
   if (value > 900) {
+>>>>>>> 135e296618deba5328bfe0e2a93004308c3bc022
     digitalWrite(LED_LIGHT, HIGH);
   } else {
     digitalWrite(LED_LIGHT,  LOW);
   }
 }
+
+
 
 // Uses the humidity sensor to check both temperature and humidity. 
 // Raises alarm if either is outside acceptable interval.
@@ -42,6 +76,8 @@ void checkTempAndHumidity() {
   int chk = DHT11.read(DHT11PIN);
   float humidity = (float)DHT11.humidity;
   float temp = (float)DHT11.temperature;
+  ThingSpeak.setField(3, humidity);  
+  ThingSpeak.setField(4, temp);
 
   if (humidity > 65 || humidity < 40){
     digitalWrite(LED_HUMID, HIGH);
@@ -64,8 +100,15 @@ void checkTempAndHumidity() {
 
 void loop()
 {
+
+  ThingSpeak.begin(client);
+  client.connect(server, 80);
+
   checkLightIntensity();
   checkTempAndHumidity();
+  
+  ThingSpeak.writeFields(channelID, APIKey);
+  client.stop();
   
   // Check the values every second.
   delay(1000);
