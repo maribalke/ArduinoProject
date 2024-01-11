135e296618deba5328bfe0e2a93004308c3bc022
colberg00 <121870624+colberg00@users.noreply.github.com>
2024-01-11 09:40:32 +0100

    Update laser_sensor.ino

--- laser_sensor.ino
+++ laser_sensor.ino
@@ -2,6 +2,7 @@ int LED_output = 3;        //LED for testing
 int laser_sensor_pin = 2;  //Pin for output from laser sensor
 int laser_sensor_value = 0;
 int laser_sensor_threshold = 700;  //Threshold for laser, lower/raise if output is wrong
+bool turn_off_alarm = false;            //Toggle the alarm, 1 for OFF
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,18 +12,18 @@ void setup() {
 }
 
 void laser_detector() {
-  laser_sensor_value = analogRead(laser_sensor_pin);
-  if (laser_sensor_value < laser_sensor_threshold) {
-    //Consequences for triggering the laser sensor
-    Serial.println("Alarm triggered");
-    digitalWrite(alarm_led_output, HIGH);
+  if (!turn_off_alarm) {
+    laser_sensor_value = analogRead(laser_sensor_pin);
+    if (laser_sensor_value < laser_sensor_threshold) {
+      //Consequences for triggering the laser sensor
+      Serial.println("Alarm triggered");
+      digitalWrite(alarm_led_output, HIGH);
+    } else {
+      //Consequence for no alarm triggered
+      Serial.println("No alarm triggered");
+      digitalWrite(alarm_led_output, LOW);
+    }
   }
-
-else {
-  //Consequence for no alarm triggered
-  Serial.println("No alarm triggered");
-  digitalWrite(alarm_led_output, LOW);
-}
 }
 
 void loop() {
